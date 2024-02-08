0b1d5b4a7fce5e860c2be9aa3559299fac19bc91
Barbara Pinheiro <bcmpinheiro@gmail.com>
2024-02-08 20:32:33 -0300

    chore: validacao ao salvar cadastro

--- src/main/java/br/com/bcmp/VerbosHttpTest.java
+++ src/main/java/br/com/bcmp/VerbosHttpTest.java
@@ -21,4 +21,19 @@ public class VerbosHttpTest {
                     .body("name", is("Jose"))
                     .body("age", is(50));
     }
+
+    @Test
+    public void naoDeveSalvarUsuarioSemNome() {
+        given()
+                    .log().all()
+                    .contentType("application/json")
+                    .body("{ \"age\": 50}")
+                .when()
+                    .post("https://restapi.wcaquino.me/users")
+                .then()
+                    .log().all()
+                    .statusCode(400)
+                    .body("id", is(nullValue()))
+                    .body("error", is("Name é um atributo obrigatório"));
+        }
 }
