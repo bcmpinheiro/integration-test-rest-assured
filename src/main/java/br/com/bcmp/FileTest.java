a1fb92a58a6278ddeecc2da666e36a9fe38b24ba
Barbara Pinheiro <bcmpinheiro@gmail.com>
2024-02-14 11:25:28 -0300

    chore: fazendo upload de arquivos

---
+++ src/main/java/br/com/bcmp/FileTest.java
@@ -0,0 +1,38 @@
+package br.com.bcmp;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class FileTest {
+
+    @Test
+    public void deveObrigarEnvioArquivo(){
+        given()
+                    .log().all()
+                .when()
+                    .post("http://restapi.wcaquino.me/upload")
+                .then()
+                    .log().all()
+                    .statusCode(404)//deveria ser 400
+                    .body("error", is("Arquivo não enviado"));
+    }
+
+    @Test
+    public void deveFazerUploadArquivo(){
+        given()
+                    .log().all()
+                    .multiPart("arquivo", new File("src/main/resources/users.pdf"))
+                .when()
+                    .post("http://restapi.wcaquino.me/upload")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("name", is("users.pdf"));
+    }
+
+}
