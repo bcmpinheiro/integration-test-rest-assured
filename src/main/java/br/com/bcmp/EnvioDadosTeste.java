251f4f9f7afee8f236213a11df7db5decf5aefe6
Barbara Pinheiro <bcmpinheiro@gmail.com>
2024-02-13 23:46:52 -0300

    chore: deve enviar dados via header

--- src/main/java/br/com/bcmp/EnvioDadosTeste.java
+++ src/main/java/br/com/bcmp/EnvioDadosTeste.java
@@ -1,9 +1,11 @@
 package br.com.bcmp;
 
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 public class EnvioDadosTeste {
 
@@ -16,8 +18,34 @@ public class EnvioDadosTeste {
                 .then()
                     .log().all()
                     .statusCode(200)
-                .contentType(ContentType.JSON)
-                    ;
+                    .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void deveEnviarDadoViaQueryViaParam() {
+        given()
+                    .log().all()
+                    .queryParam("format", "json")
+                .when()
+                    .get("https://restapi.wcaquino.me/v2/users")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .contentType(containsString("utf-8"))
+                    .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void deveEnviarDadoViaHeader() {
+        given()
+                    .log().all()
+                    .accept(ContentType.JSON)
+                .when()
+                    .get("https://restapi.wcaquino.me/v2/users")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .contentType(ContentType.HTML);
     }
 
 }
