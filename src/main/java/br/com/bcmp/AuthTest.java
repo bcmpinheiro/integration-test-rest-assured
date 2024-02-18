de9c486495a630949a45ab0c6685c1f67639cf06
Barbara Pinheiro <bcmpinheiro@gmail.com>
2024-02-17 22:03:48 -0300

    chore: add validacao api sw

--- src/main/java/br/com/bcmp/AuthTest.java
+++ src/main/java/br/com/bcmp/AuthTest.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
 
 public class AuthTest {
@@ -18,10 +19,68 @@ public class AuthTest {
                     .log().all()
                     .contentType(ContentType.JSON)
                     .statusCode(200)
-                    .body("name", is("Luke Skywalker"))
-                ;
-
+                    .body("name", is("Luke Skywalker"));
     }
 
+    @Test
+    public void deveValidarApi() {
+        given()
+                    .log().all()
+                .when()
+                    .get("https://api.adviceslip.com/advice/5")
+                .then()
+                    .log().all()
+                    .contentType(ContentType.HTML)
+                    .statusCode(200)
+                    .body("html.body.size()", is(1));
+    }
 
+    @Test
+    public void naoDeveAcessarSemSenha() {
+        given()
+                    .log().all()
+                .when()
+                    .get("https://restapi.wcaquino.me/basicauth")
+                .then()
+                    .log().all()
+                    .statusCode(401);
+    }
+
+    @Test
+    public void deveFazerAutenticacaoBasica() {
+        given()
+                    .log().all()
+                .when()
+                    .get("https://admin:senha@restapi.wcaquino.me/basicauth")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("status", is ("logado"));
+    }
+
+    @Test
+    public void deveFazerAutenticacaoBasica2() {
+        given()
+                    .log().all()
+                .auth().basic("admin", "senha")
+                .when()
+                    .get("https://restapi.wcaquino.me/basicauth")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("status", is ("logado"));
+    }
+
+    @Test
+    public void deveFazerAutenticacaoBasicaChallenge() {
+        given()
+                    .log().all()
+                    .auth().preemptive().basic("admin", "senha")
+                .when()
+                    .get("https://restapi.wcaquino.me/basicauth2")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("status", is ("logado"));
+    }
 }
