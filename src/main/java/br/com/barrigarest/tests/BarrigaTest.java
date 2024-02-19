948ba9108d5030b4a3938eb5904398e9afcf6ee0
Barbara Pinheiro <bcmpinheiro@gmail.com>
2024-02-18 22:02:15 -0300

    chore: add alteracao de conta criada

--- src/main/java/br/com/barrigarest/tests/BarrigaTest.java
+++ src/main/java/br/com/barrigarest/tests/BarrigaTest.java
@@ -1,13 +1,30 @@
 package br.com.barrigarest.tests;
 
 import br.com.barrigarest.core.BaseTest;
-import io.restassured.http.ContentType;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class BarrigaTest extends BaseTest {
+
+    private String TOKEN;
+    @Before
+    public void login() {
+        Map<String,String> login = new HashMap<>();
+        login.put("email", "barbara@barbara");
+        login.put("senha", "123456");
+
+        TOKEN = given()
+                .body(login)
+                .when()
+                .post("/signin")
+                .then()
+                .statusCode(200)
+                .extract().path("token");
+    }
 
     @Test
     public void naoDeveAcessarApiSemToken() {
@@ -16,6 +33,30 @@ public class BarrigaTest extends BaseTest {
                     .get("/contas")
                 .then()
                     .statusCode(401);
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso() {
+        given()
+                    .header("Authorization", "JWT " + TOKEN)
+                    .body("{\"nome\": \"teste2\"}")
+                .when()
+                    .post("/contas")
+                .then()
+                    .statusCode(201);
+    }
+
+    @Test
+    public void deveAlterarContaComSucesso() {
+        given()
+                    .header("Authorization", "JWT " + TOKEN)
+                    .body("{\"nome\": \"conta teste n1\"}")
+                .when()
+                    .put("/contas/2041183")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("nome", is("conta teste n1"));
     }
 
 
